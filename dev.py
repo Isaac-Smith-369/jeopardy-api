a24076dcba4616ebb873d9478f3afccb7f9a4365
Isaac-Smith-369 <sagesmith369@gmail.com>
2023-09-08 07:42:47 -0700

    redeploy

--- dev.py
+++ dev.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI
+import uvicorn
+from os import getenv
 
 
-app = FastAPI()
+if __name__ == "__main__":
+    port = int(getenv("PORT", 8000))
+    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
 
-
-@app.get("/")
-def index():
-    return "Welcome to jeopardy"
