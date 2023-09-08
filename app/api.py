cf37fa869f93b21bb8f027e899e2281af2c0f9ac
Isaac-Smith-369 <sagesmith369@gmail.com>
2023-09-08 07:14:53 -0700

    Refactored the code and removed unused dependencies

--- app/api.py
+++ app/api.py
@@ -8,62 +8,18 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.level = logging.DEBUG
 
-CREATE_JEOPARDY_TABLE = """
-    CREATE TABLE jeopardy (
-        id INTEGER PRIMARY KEY,
-        show_number TEXT,
-        air_date DATE,
-        round TEXT,
-        category TEXT,
-        value INTEGER,
-        question TEXT,
-        answer TEXT
-    );
-"""
 
-INSERT_INTO_JEOPARDY = """
-    INSERT INTO jeopardy (
-        show_number,
-        air_date,
-        round,
-        category,
-        value,
-        question,
-        answer
-    )
-    VALUES (?, ?, ?, ?, ?, ?, ?);
-"""
-
-GET_JEOPARDY_CATEGORIES = """
-    SELECT category FROM jeopardy
-    ORDER BY RANDOM();
-"""
-
-GET_JEOPARARDY_QUESTIONS = """
-    SELECT
-        id,
-        round,
-        category,
-        value,
-        question,
-        answer
-    FROM jeopardy
-    WHERE category = ?
-    ORDER BY value;
-"""
-
-# CWD = Path(__file__).parent.parent
-# CSV_PATH = CWD / "data/jeopardy.csv"
-# DB_PATH = CWD / "data/jeopardy.db"
+CWD = Path(__file__).parent.parent
+DB_PATH = CWD / "data/jeopardy.db"
 
 
-# def game():
-#     result = get_jeopardy_round(GET_JEOPARDY_CATEGORIES, GET_JEOPARARDY_QUESTIONS, DB_PATH)
-#     if result is None:
-#         logging.warning("No questions available")
-#         return
-#     game = Game.model_validate(result)
-#     return game
+def game():
+    result = get_jeopardy_round(DB_PATH)
+    if result is None:
+        logging.warning("Couldn't retrieve jeopardy round")
+        return
+    game = Game.model_validate(result)
+    return game
 
 
 app = FastAPI()
@@ -73,6 +29,6 @@ app = FastAPI()
 def index():
     return "Welcome to jeopardy"
 
-# @app.get("/game")
-# def get_game():
-#     return game()
+@app.get("/game")
+def get_game():
+    return game()
--- app/queries.py
+++ app/queries.py
@@ -1,57 +1,8 @@
 import logging
 from app.db import DB
 from pathlib import Path
-from typing import Any, Hashable
+from typing import Any
 from app.models import Question
-from app.utils import parse_jeopardy_csv
-
-
-def create_table(query: str, source: Path):
-    logging.info("Creating database table")
-    with DB(str(source)) as (_, cursor):
-        try:
-            cursor.execute(query)
-            logging.info("Finished creating table")
-        except Exception:
-            logging.exception("Failed to create database")
-
-def insert_into_table(query: str, source: Path, data: list[tuple[Any]]):
-    logging.info("Inserting data into database")
-    with DB(str(source)) as (connection, cursor):
-        try:
-            for record in data:
-                cursor.execute(query, record)
-                connection.commit()
-            logging.info("Finished inserting data")
-        except Exception:
-            logging.exception("Failed to insert into database")
-
-def seed_jeopardy_db(query: str, db_path: Path, csv_path: Path):
-    logging.info("Seeding jeopardy database")
-    records = []
-
-    data = parse_jeopardy_csv(csv_path)
-
-    for i, d in enumerate(data):
-
-        if i == 1005:
-            break
-
-        # print(d["Value"])
-        value: int = int(d["Value"].replace("$", "").replace(",", "")) if isinstance(d["Value"], str) else 200
-        tmp = (
-            d["Show Number"],
-            d["Air Date"],
-            d["Round"],
-            d["Category"],
-            value,
-            d["Question"],
-            d["Answer"]
-        )
-        records.append(tmp)
-
-    insert_into_table(query, db_path, records)
-    logging.info("Finished seeding jeopardy database")
 
 
 def get_jeopardy_data(query: str, source: Path, limit: int = 2) -> list[tuple[Any]] | None:
@@ -63,6 +14,7 @@ def get_jeopardy_data(query: str, source: Path, limit: int = 2) -> list[tuple[An
             return result
         except Exception:
             logging.exception("Failed to retrieve jeopardy data")
+
 
 def get_jeopardy_questions(query: str, source: Path, category: tuple[str], limit: int = 2) -> list[Question] | None:
     logging.info("Retrieving jeopardy questions")
@@ -76,10 +28,28 @@ def get_jeopardy_questions(query: str, source: Path, category: tuple[str], limit
             logging.exception("Failed to retrieve jeopardy questions")
 
 
-def get_jeopardy_round(categories_query: str, questions_query: str, source: Path) -> dict[str, list[list[Question]] | int] | None:
+def get_jeopardy_round(source: Path) -> dict[str, list[list[Question]] | int] | None:
+    GET_JEOPARDY_CATEGORIES = """
+        SELECT category FROM jeopardy
+        ORDER BY RANDOM();
+    """
+
+    GET_JEOPARARDY_QUESTIONS = """
+        SELECT
+            id,
+            round,
+            category,
+            value,
+            question,
+            answer
+        FROM jeopardy
+        WHERE category = ?
+        ORDER BY value;
+    """
+
     logging.info("Retrieving jeopardy round")
     # get 5 random categories and then use those categories to fetch the rounds
-    categories = get_jeopardy_data(categories_query, source, 3)
+    categories = get_jeopardy_data(GET_JEOPARDY_CATEGORIES, source, 3)
 
     if categories is None:
         logging.warning("No categories available")
@@ -90,7 +60,7 @@ def get_jeopardy_round(categories_query: str, questions_query: str, source: Path
 
     for category in categories:
 
-        questions_set = get_jeopardy_questions(questions_query, source, category, 5)
+        questions_set = get_jeopardy_questions(GET_JEOPARARDY_QUESTIONS, source, category, 5)
 
         if questions_set is None:
             logging.warning("No questions available for these categories")
