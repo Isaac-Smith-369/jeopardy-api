cac903b4b6d02138e20fd301f9b67feb296a51c4
Isaac-Smith-369 <sagesmith369@gmail.com>
2023-09-08 07:51:36 -0700

    redeploy

--- app/api.py
+++ app/api.py
@@ -1,25 +1,27 @@
-from pathlib import Path
-# import logging
-# from app.models import Game
-# from app.queries import get_jeopardy_round
+# from pathlib import Path
+import logging
+from app.models import Game
+from app.queries import get_jeopardy_round
 from fastapi import FastAPI
 
-# logging.basicConfig()
-# logger = logging.getLogger(__name__)
-# logger.level = logging.DEBUG
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.level = logging.DEBUG
 
 
 # CWD = Path(__file__).parent.parent
 # DB_PATH = CWD / "data/jeopardy.db"
 
 
-# def game():
-#     result = get_jeopardy_round(DB_PATH)
-#     if result is None:
-#         logging.warning("Couldn't retrieve jeopardy round")
-#         return
-#     game = Game.model_validate(result)
-#     return game
+def game():
+    # result = get_jeopardy_round(DB_PATH)
+    # if result is None:
+    #     logging.warning("Couldn't retrieve jeopardy round")
+    #     return
+    # game = Game.model_validate(result)
+    # return game
+    logging.info("This works")
+    return "This also works"
 
 
 app = FastAPI()
@@ -29,6 +31,6 @@ app = FastAPI()
 def index():
     return "Welcome to jeopardy"
 
-# @app.get("/game")
-# def get_game():
-#     return game()
+@app.get("/game")
+def get_game():
+    return game()
