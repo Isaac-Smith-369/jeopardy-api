1f7930ce1181144c750a881c5d38e37c4448b61c
Isaac-Smith-369 <sagesmith369@gmail.com>
2023-09-08 07:44:27 -0700

    redeploy

--- app/api.py
+++ app/api.py
@@ -1,34 +1,34 @@
-from pathlib import Path
-import logging
-from app.models import Game
-from app.queries import get_jeopardy_round
-from fastapi import FastAPI
+# from pathlib import Path
+# import logging
+# from app.models import Game
+# from app.queries import get_jeopardy_round
+# from fastapi import FastAPI
 
-logging.basicConfig()
-logger = logging.getLogger(__name__)
-logger.level = logging.DEBUG
-
-
-CWD = Path(__file__).parent.parent
-DB_PATH = CWD / "data/jeopardy.db"
+# logging.basicConfig()
+# logger = logging.getLogger(__name__)
+# logger.level = logging.DEBUG
 
 
-def game():
-    result = get_jeopardy_round(DB_PATH)
-    if result is None:
-        logging.warning("Couldn't retrieve jeopardy round")
-        return
-    game = Game.model_validate(result)
-    return game
+# CWD = Path(__file__).parent.parent
+# DB_PATH = CWD / "data/jeopardy.db"
 
 
-app = FastAPI()
+# def game():
+#     result = get_jeopardy_round(DB_PATH)
+#     if result is None:
+#         logging.warning("Couldn't retrieve jeopardy round")
+#         return
+#     game = Game.model_validate(result)
+#     return game
 
 
-@app.get("/")
-def index():
-    return "Welcome to jeopardy"
+# app = FastAPI()
 
-@app.get("/game")
-def get_game():
-    return game()
+
+# @app.get("/")
+# def index():
+#     return "Welcome to jeopardy"
+
+# @app.get("/game")
+# def get_game():
+#     return game()
